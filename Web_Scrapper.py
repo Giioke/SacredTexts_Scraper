6b62b1e4993a3ea5c016b44cef7b2058bf2ca21f
Giioke <45025935+Giioke@users.noreply.github.com>
2019-07-06 14:29:27 -0400

    Simplified the topic extraction loop so that it collects into a varible names stripped_Topicmenu. However this version is not as cleanly printed. All data also seems to be placed within the 0 index.

--- Web_Scrapper.py
+++ Web_Scrapper.py
@@ -2,7 +2,7 @@
 # Source - https://www.youtube.com/watch?v=7SWVXPYZLJM&t=397s
 import requests
 from bs4 import BeautifulSoup
-5465
+
 url = "http://www.sacred-texts.com/index.htm"
 resp = requests.get(url)
 
@@ -11,31 +11,31 @@ soup = BeautifulSoup(resp.text, 'html.parser')
 
 # Find the names and links to the books in the website's menu elements
 topicMenu = soup.select(".menutext")
-#print(topicMenu[1])
+
 #Extract topic names
-for topic in topicMenu:
-    print(topic.text.strip())
+#for topic in topicMenu:
+    #print(topic.text.strip())
+stripped_Topicmenu = [topic.text.strip() for topic in topicMenu]
+print(stripped_Topicmenu)
 
-#####
-#Data Structure
-topics = {
-    "topic1": {
-        "descript": "content",
-        "topicLink": "weblink"
-    },
-    "topic2": {
-        "descript": "content",
-        "topicLink": "weblink"
+## Data Structure
+# topics = {
+#     "topic1": {
+#         "descript": "content",
+#         "topicLink": "weblink"
+#     },
+#     "topic2": {
+#         "descript": "content",
+#         "topicLink": "weblink"
 
-    }
-#Add more topics?
-}
-#####
+#     }
+## Add more topics?
+# }
 
-#For-Loop each topic displays their name, description, and the link.
-for topicname, topicinfo in topics.items():
-    print("\n")
-    print(topicname)
-    print(topicinfo['descript'])
-    print(topicinfo['topicLink'])
-    print("\n")
+##For-Loop each topic displays their name, description, and the link.
+# for topicname, topicinfo in topics.items():
+#     print("\n")
+#     print(topicname)
+#     print(topicinfo['descript'])
+#     print(topicinfo['topicLink'])
+#     print("\n")
