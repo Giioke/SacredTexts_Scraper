8d7c8a799dee59269b9e666ed6254c281a81c9ad
Giioke <45025935+Giioke@users.noreply.github.com>
2019-07-06 13:02:21 -0400

    Added comments/ Selected the topic menu and extracted topic text.

--- Web_Scrapper.py
+++ Web_Scrapper.py
@@ -6,5 +6,12 @@ from bs4 import BeautifulSoup
 url = "http://www.sacred-texts.com/index.htm"
 resp = requests.get(url)
 
+#Ability to read the HTML in python
 soup = BeautifulSoup(resp.text, 'html.parser')
-print(soup)
+
+# Find the names and links to the books in the website's menu elements
+topicMenu = soup.select(".menutext")
+
+#Extract topic names
+for topic in topicMenu:
+    print(topic.text.strip())
